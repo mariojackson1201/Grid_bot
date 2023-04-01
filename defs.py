118400fe668fa16f1bddcd4010829c3c34ec3644
jcjstuga <jcjssantos@gmail.com>
2023-04-01 20:12:32 +0100

    initial commit

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "0046d0ab15e0c0dfee6eb9ba76e92a0e-536fe1ae1c9129243bd08f89208f5470"
+ACCOUNT_ID = "101-004-21205389-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
